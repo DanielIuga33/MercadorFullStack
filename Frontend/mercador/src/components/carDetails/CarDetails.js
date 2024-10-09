610a20c6082f66b7b3716f7d05515e93938a31a5
Daniel Iuga <danieliuga33@yahoo.com>
2024-10-10 00:52:26 +0300

    still working on carDetails

--- Frontend/mercador/src/components/carDetails/CarDetails.js
+++ Frontend/mercador/src/components/carDetails/CarDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
@@ -10,6 +10,9 @@ const CarDetails = ({carDataId}) => {
   const [car, setCar] = useState({});
   const [carImages, setCarImages] = useState([]); // Inițializează cu array gol
   const [loading, setLoading] = useState(true); // Stare pentru loading
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slider1Ref = useRef(null);
+  const slider2Ref = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,6 +30,7 @@ const CarDetails = ({carDataId}) => {
     fetchData();
   },[carDataId]);
 
+
   if (loading) {
     return (
         <div className='bg'>
@@ -35,14 +39,35 @@ const CarDetails = ({carDataId}) => {
     );
   }
 
-  const settings = {
-    dots: true,
+  const settings1 = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    beforeChange: (oldIndex, newIndex) => {
+        setCurrentSlide(newIndex);
+        // Schimbăm și slider-ul 2 când se schimbă slider-ul 1
+        if (slider2Ref.current) {
+          slider2Ref.current.slickGoTo(newIndex);
+        }
+      },
   };
+
+  const settings2 = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+  const handleImageClick = (index) => {
+    if (slider1Ref.current && slider2Ref.current) {
+      slider1Ref.current.slickGoTo(index); // Schimbă slide-ul la slider-ul mare
+      slider2Ref.current.slickGoTo(index); // Schimbă slide-ul la slider-ul mic
+    }
+  };
+
 
   return (
     <div className='car-details'>
@@ -51,23 +76,43 @@ const CarDetails = ({carDataId}) => {
                 <h1>{car.title}</h1>
             </span>
             <div className="car-image-slider">
-                <Slider {...settings}>
+                <Slider ref={slider1Ref} {...settings1}>
                     {carImages.map((img, index) => (
-                        <div className='image' key={index}>
+                        <div className='image' key={index} onClick={() => handleImageClick(index)}>
                             <img src={`http://localhost:8080/api${img}`} alt={`Car ${index + 1}`} />
                         </div>
                     ))}
                 </Slider>
+                <div className="car-images-slider">
+                    <Slider ref={slider2Ref} {...settings2}>
+                        {carImages.map((img, index) => (
+                            <div className='image' key={index} onClick={() => handleImageClick(index)}>
+                                <img src={`http://localhost:8080/api${img}`} alt={`Car ${index + 1}`} />
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
+                <h5>{`${currentSlide+1} / ${carImages.length}`}</h5>
             </div>
             <div className='car-info-box'>
-                <div className='col'>
-                    <label><em>An fabricatie: </em><p>{car.year}</p></label>
+                <div className='spacer'></div>
+                <div className='row'>
+                    <h4>Price</h4>
+                    <div className='bar'></div>
+                    {car.currency !== "€" && 
+                    <label><span className="align-right"><h4>{car.price} {car.currency} </h4></span><h4>{car.price * 5 } RON</h4></label>}
                 </div>
-                <div className='col'>
-                  <label><em>An fabricatie: </em><p>{car.year}</p></label>
+                <div className='row'>
+                    <h4>Tehnical Data</h4>
+                    <div className='bar'></div>
+                    <label><span className="align-right"><strong>Condition: </strong></span>{car.condition}</label>
+                    <label><span className="align-right"><strong>Body type: </strong></span>{car.body}</label>
+                    <label><span className="align-right"><strong>First registartion: </strong></span>{car.year}</label>
+                    <label><span className="align-right"><strong>Gearbox: </strong></span>Transmission {car.transmission}</label>
+                    <label><span className="align-right"><strong>Fuel: </strong></span>{car.fuelType}</label>
+                    <label><span className="align-right"><strong>Mileage: </strong></span>{car.mileage} km</label>
                 </div>
-                <div className='col'>
-                  
+                <div className='row'>
                 </div>
             </div>
        </div>
